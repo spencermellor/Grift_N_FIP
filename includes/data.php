d17bd783d2d8e8825bb01b3def57a55f194785bc
nategrift <nategrift@gmail.com>
2020-11-23 21:24:39 -0500

    Added ability to query and readme

--- includes/data.php
+++ includes/data.php
@@ -1,14 +1,32 @@
 <?php
 
-include_once '../config/database.php';
+// ini_set('display_errors', 1);
 
-$database = new Database();
-$db = $database->getConnection();
-
+// required headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-$results = [];
+// Includes database and porfolio object
+include_once '../config/database.php';
+include_once '../objects/portfolio.php';
 
-echo json_encode($results);
+// Database connection
+$database = new Database();
+$db = $database->getConnection();
+
+// Create Portfolio object
+$portfolio = new Portfolio($db);
+
+
+if (isset($_GET['id'])) {
+    $result = $portfolio->getPieceByID($_GET['id']);
+} elseif (isset($_GET['category'])) {
+    $result = $portfolio->getPiecesByCategory($_GET['category']);
+} elseif (isset($_GET['tag'])) {
+    $result = $portfolio->getPieceByTag($_GET['tag']);
+} else {
+    $result = $portfolio->getPieces();
+}
+
+echo json_encode($result);
 exit;
